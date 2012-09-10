ee992d42903f2ca0e5b4be1a6d2a6e8b9e87526f
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-09-10 22:18:56 +0100

    simple server

--- server.js
+++ server.js
@@ -1,9 +1,75 @@
 #!/usr/local/bin/node
 
-var http = require('http');
+var PORT = 1234;
+var ADDRESS = '127.0.0.1';
 
-var server = http.createServer(function(req, res){
-	console.log(req);
+
+var path = require('path');
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+var mappings = {
+	'html' : 'text/html',
+	'js'   : 'text/javascript'
+};
+
+var server = http.createServer();
+
+server.on('request', function(request, response){
+	console.log('* Server.Request');
+
+	request.on('data', function(chunk){
+
+	});
+	request.on('end', function(){
+
+	});
+	request.on('close', function(){
+
+	});
+
+	console.log('** request.method: ' + request.method);
+	var parsed = url.parse(request.url);
+	console.log('** request.url: ' + request.url, parsed);
+
+	fs.readFile('.' + parsed.pathname, function(err, data){
+		if(err) {
+
+		} else {
+			response.writeHead(200, {
+				  'Content-Length': data.length,
+				  'Content-Type': 'text/plain' 
+			});
+			response.write(data);
+			response.end();
+		}
+	});
 });
 
-server.listen(1234, '127.0.0.1');
+server.on('connection', function(socket){
+	console.log('* Server.Connection');
+});
+
+server.on('close', function(){
+	console.log('* Server.Close');
+});
+
+server.on('checkContinue', function(request, response){
+	console.log('* Server.CheckContinue');
+});
+
+server.on('connect', function(request, socket, head){
+	console.log('* Server.Connect');
+});
+
+server.on('upgrade', function(request, socket, head){
+	console.log('* Server.Upgrade');
+});
+
+server.on('clientError', function(exception){
+	console.log('* Server.ClientError');
+});
+
+server.listen(PORT, ADDRESS);
+
