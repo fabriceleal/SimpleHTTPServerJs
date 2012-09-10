91a7b860bbc195bb3353375a1f895e086b97cbe8
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-09-10 22:28:25 +0100

    serves .js and .html. Good enough for me!

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ var fs = require('fs');
 var url = require('url');
 
 var mappings = {
-	'html' : 'text/html',
-	'js'   : 'text/javascript'
+	'.html' : 'text/html',
+	'.js'   : 'text/javascript'
 };
 
 var server = http.createServer();
@@ -33,18 +33,41 @@ server.on('request', function(request, response){
 	var parsed = url.parse(request.url);
 	console.log('** request.url: ' + request.url, parsed);
 
-	fs.readFile('.' + parsed.pathname, function(err, data){
-		if(err) {
+	// Is this dangerous because of '..' ???
+	var normalized = '.' + parsed.pathname;
+	console.log('** normalized: ' + normalized);
 
-		} else {
-			response.writeHead(200, {
-				  'Content-Length': data.length,
-				  'Content-Type': 'text/plain' 
-			});
-			response.write(data);
-			response.end();
-		}
-	});
+	var resourceType = mappings[path.extname(normalized)];
+	console.log('** resourceType: ' + resourceType);
+
+	if(resourceType) {
+
+		fs.readFile(normalized, function(err, data){
+			if(err) {
+				console.log('*** Some error serving url "' + request.url + '", 404!');
+				console.log(err);
+
+				response.writeHead(404);
+				response.end();
+			} else {
+				console.log('*** Serving url "' + request.url + '"');
+				response.writeHead(200, {
+					  'Content-Length': data.length,
+					  'Content-Type': resourceType
+				});
+				response.write(data);
+				response.end();
+			}
+		});
+
+	} else {
+		console.log('*** This file extension is not served, 404!');
+		// This file is not served!
+		response.writeHead(404);
+		response.end();
+	}
+
+
 });
 
 server.on('connection', function(socket){
